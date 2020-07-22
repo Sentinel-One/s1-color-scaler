04cf41144f30a003cb99e6033c4ccf01cd7a35c9
lironh <lironenk@gmail.com>
2020-07-22 07:39:43 +0300

    chore(color-scaler): rename

--- projects/s1-graphic-capsules/src/public-api.ts
+++ projects/s1-graphic-capsules/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API
  */
 // changes
-export * from './lib/s1-color-scaler/s1-color-scaler';
-export * from './lib/s1-color-scaler/color-grabber.directive';
-export * from './lib/s1-color-scaler/s1-color-scaler.module';
+export * from './lib/color-scaler/s1-color-scaler';
+export * from './lib/color-scaler/color-grabber.directive';
+export * from './lib/color-scaler/s1-color-scaler.module';
